10c1d52bcf2765a7260773c884581fde7b1f2563
unknown <leslie2000abayo@gmail.com>
2025-06-21 20:18:27 +0300

    Added the custom tab successfully

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import QrCodeGenerator from "./Components/Qr-Code-Generator";
 import Colorgenerator from "./Components/Random-Color-Generator";
 import ScrollIndicator from "./Components/Scroll-indicator";
 import StarRating from "./Components/Star-Rating";
+import Tabs from "./Components/Tabs";
 import Treeview from "./Components/Tree-view";
 import data from "./Components/Tree-view/data";
  
@@ -21,6 +22,7 @@ function App() {
       <LoadMoreButton url="https://dummyjson.com/products"/>
       <ImageSlider url="https://picsum.photos/v2/list" limit="10" page="3"/>
       <ScrollIndicator url={"https://dummyjson.com/products?limit=100"} />
+      <Tabs/>
     </div>
   );
 }
---
+++ src/Components/Tabs/index.jsx
@@ -0,0 +1,33 @@
+import './styles-9.css';
+import { useState } from 'react';
+
+const Tabs = () => {
+    const [ clicked,setClicked ] = useState(false);
+    const [ tabValue, setTabValue ] = useState("");
+
+    function handleClick(e){
+      const string = e.target.textContent;
+      console.log(e.target.textContent)
+
+      const tabNumber = string.split(' ')[1]
+      setTabValue(tabNumber);
+      setClicked(true);
+    }
+
+  return (
+    <div className="container">
+       <div className="tabs-container mb-2"> 
+         <button className={ tabValue === "1" && clicked ? "p-2 btn btn-danger rounded-3" :"p-2 btn btn-success rounded-3"} onClick={(e) => handleClick(e)}>Tab 1</button>
+         <button className={ tabValue === "2" && clicked ? "p-2 btn btn-danger rounded-3" :"p-2 btn btn-success rounded-3"} onClick={(e) => handleClick(e)}>Tab 2</button>
+         <button className={ tabValue === "3" && clicked ? "p-2 btn btn-danger rounded-3" :"p-2 btn btn-success rounded-3"} onClick={(e) => handleClick(e)}>Tab 3</button>
+        </div>
+
+        <div className="text-container">
+        { tabValue === "1" && <p className="fs-3 text-danger">This is content is for Tab 1</p> }
+        { tabValue === "2" && <p className="fs-3 text-danger">Content for tab 2.</p> }
+        { tabValue === "3" && <p className="fs-3 text-danger">Some random content.</p> }
+        </div>
+    </div>
+  )
+}
+export default Tabs
